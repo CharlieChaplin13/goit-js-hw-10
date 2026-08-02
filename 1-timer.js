3624df681173d347e9bf01827755c4ff35ee5854
CharlieChaplin13 <101952904+CharlieChaplin13@users.noreply.github.com>
2026-08-02 19:47:03 +0000

    Deploying to gh-pages from @ CharlieChaplin13/goit-js-hw-10@5ceb67fe59d60f0f7363e7e5c6169d2728b6be58 🚀

--- 1-timer.js
+++ 1-timer.js
@@ -1,20 +1,20 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{f}from"./assets/vendor-EyZmBGcZ.js";const n=document.querySelector("button[data-start]"),m=document.querySelector(".timer");n.addEventListener("click",h);let c=!1,a=null;const v={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(s){console.log(s[0]),a=s[0],n.disabled=!1}};f("#datetime-picker",v);y();function h(){if(!c){if(!a){alert("Please choose a date first!");return}c=!0,n.disabled=!0,setInterval(()=>{const s=Date.now(),t=a-s,o=i(t);l(o)},1e3)}}function l(s){m.innerHTML=`
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{f as p}from"./assets/vendor-EyZmBGcZ.js";const s=document.querySelector("button[data-start]"),m=document.querySelector(".timer");s.addEventListener("click",h);let c=!1,a=null;const v={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0]),a=e[0];let t=new Date;if(a.getTime()<t.getTime()){window.alert("Please choose a date in the future");return}else s.disabled=!1;return s.disabled}};p("#datetime-picker",v);y();function h(){if(!c){if(!a){alert("Please choose a date first!");return}c=!0,s.disabled=!0,setInterval(()=>{const e=Date.now(),n=a-e,t=i(n);l(t)},1e3)}}function l(e){m.innerHTML=`
     <div class="timer">
       <div class="field">    
-        <span class="value" data-days>${e(s.days)}</span>
+        <span class="value" data-days>${o(e.days)}</span>
         <span class="label">Days</span>
       </div>
       <div class="field">
-        <span class="value" data-hours>${e(s.hours)}</span>
+        <span class="value" data-hours>${o(e.hours)}</span>
         <span class="label">Hours</span>
       </div>
       <div class="field">
-        <span class="value" data-minutes>${e(s.minutes)}</span>
+        <span class="value" data-minutes>${o(e.minutes)}</span>
         <span class="label">Minutes</span>
       </div>
       <div class="field">
-        <span class="value" data-seconds>${s.seconds}</span>
+        <span class="value" data-seconds>${e.seconds}</span>
         <span class="label">Seconds</span>
       </div>
-    </div>`}function y(){const s=i(0);l(s)}function i(s){const r=Math.floor(s/864e5),d=Math.floor(s%864e5/36e5),u=Math.floor(s%864e5%36e5/6e4),p=Math.floor(s%864e5%36e5%6e4/1e3);return{days:r,hours:d,minutes:u,seconds:p}}function e(s){return String(s).padStart(2,0)}
+    </div>`}function y(){const e=i(0);l(e)}function i(e){const r=Math.floor(e/864e5),d=Math.floor(e%864e5/36e5),u=Math.floor(e%864e5%36e5/6e4),f=Math.floor(e%864e5%36e5%6e4/1e3);return{days:r,hours:d,minutes:u,seconds:f}}function o(e){return String(e).padStart(2,0)}
 //# sourceMappingURL=1-timer.js.map
