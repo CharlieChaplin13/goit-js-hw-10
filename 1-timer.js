366af69da6cce4d66b4eaa1ae431b7488ef85c15
CharlieChaplin13 <101952904+CharlieChaplin13@users.noreply.github.com>
2026-08-02 23:09:20 +0000

    Deploying to gh-pages from @ CharlieChaplin13/goit-js-hw-10@ac5ad15969e0181408d0b849b252ee68ab7be260 🚀

--- 1-timer.js
+++ 1-timer.js
@@ -1,20 +1,2 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{f as p}from"./assets/vendor-EyZmBGcZ.js";const s=document.querySelector("button[data-start]"),m=document.querySelector(".timer");s.addEventListener("click",h);let c=!1,a=null;const v={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0]),a=e[0];let t=new Date;if(a.getTime()<t.getTime()){window.alert("Please choose a date in the future");return}else s.disabled=!1;return s.disabled}};p("#datetime-picker",v);y();function h(){if(!c){if(!a){alert("Please choose a date first!");return}c=!0,s.disabled=!0,setInterval(()=>{const e=Date.now(),n=a-e,t=i(n);l(t)},1e3)}}function l(e){m.innerHTML=`
-    <div class="timer">
-      <div class="field">    
-        <span class="value" data-days>${o(e.days)}</span>
-        <span class="label">Days</span>
-      </div>
-      <div class="field">
-        <span class="value" data-hours>${o(e.hours)}</span>
-        <span class="label">Hours</span>
-      </div>
-      <div class="field">
-        <span class="value" data-minutes>${o(e.minutes)}</span>
-        <span class="label">Minutes</span>
-      </div>
-      <div class="field">
-        <span class="value" data-seconds>${e.seconds}</span>
-        <span class="label">Seconds</span>
-      </div>
-    </div>`}function y(){const e=i(0);l(e)}function i(e){const r=Math.floor(e/864e5),d=Math.floor(e%864e5/36e5),u=Math.floor(e%864e5%36e5/6e4),f=Math.floor(e%864e5%36e5%6e4/1e3);return{days:r,hours:d,minutes:u,seconds:f}}function o(e){return String(e).padStart(2,0)}
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{f as h,i as d}from"./assets/vendor-BbSUbo7J.js";const s=document.querySelector("button[data-start]"),y=document.querySelector("span[data-days]"),S=document.querySelector("span[data-hours]"),g=document.querySelector("span[data-minutes]"),T=document.querySelector("span[data-seconds]");s.disabled=!0;s.addEventListener("click",b);let c=!1,i=null,r=null;const D={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(t){r=t[0];let e=new Date;r.getTime()<e.getTime()?(d.error({title:"Error",message:"Please choose a date in the future",position:"topRight"}),s.disabled=!0):s.disabled=!1}};h("#datetime-picker",D);function b(){if(c)return;if(!r){alert("Please choose a date first!");return}c=!0,s.disabled=!0;const t=r.getTime();i=setInterval(()=>{const o=Date.now(),e=t-o;if(e<=0){clearInterval(i),c=!1,u(0,0,0,0),d.success({title:"Success",message:"Time is up!",position:"topRight"});return}const n=v(e);u(n.days,n.hours,n.minutes,n.seconds)},1e3)}function u(t,o,e,n){y.textContent=a(t),S.textContent=a(o),g.textContent=a(e),T.textContent=a(n)}function v(t){const l=Math.floor(t/864e5),m=Math.floor(t%864e5/36e5),f=Math.floor(t%864e5%36e5/6e4),p=Math.floor(t%864e5%36e5%6e4/1e3);return{days:l,hours:m,minutes:f,seconds:p}}function a(t){return String(t).padStart(2,0)}
 //# sourceMappingURL=1-timer.js.map
